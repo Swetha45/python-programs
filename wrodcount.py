ecca7699bc2a9195955505df451e1de13a710d94
Swetha Manathkar <61093368+python-practise@users.noreply.github.com>
2020-04-17 11:54:55 -0700

    Delete wrodcount.py

--- wrodcount.py
+++
@@ -1,6 +0,0 @@
-Working amid coronavirus restrictions, engineers readied a Soyuz spacecraft for launch Thursday to ferry two Russian cosmonauts and Navy SEAL-turned-NASA-astronaut Chris Cassidy to the International Space Station.
-
-
-Liftoff from the Baikonur Cosmodrome in Kazakhstan is targeted for 4:05:06 a.m. EDT (1:05 p.m. local time). It will be the first piloted flight of an upgraded Soyuz 2.1a booster following a series of successful uncrewed launchings.
-
-In a stark departure from normal routine, the usual crowd of family, friends and other well-wishers was not on hand at the sprawling spaceport to cheer the crew on, thanks to travel restrictions intended to slow the spread of the COVID-19 coronavirus.
