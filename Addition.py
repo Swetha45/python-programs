e876887e477605f10775ac762bdc5781eade0c4f
Sweth121 <swetham@tourmalinelabs.com>
2020-08-08 17:30:19 -0700

    -addition of two numbers

--- Addition.py
+++ Addition.py
@@ -1,21 +1,15 @@
 
 # addition of two numbers
-
-a= 20.5
-b= 30.5
-c = a + b
-print("The addition of two numbers are",  c)
+#
+# a= 20.5
+# b= 30.5
+# c = a + b
+# print("The addition of two numbers are",  c)
 
 
 # user input
-def addition(Value1, Value2):
-    if Value1.isdigit() and Value2.isdigit():
-        print( "The Addition of {0} and {1} is {2}".format( Value1, Value2, Value3 ) )
-        print( "The given value is integer:", isinstance( Value3, int ) )
-    else:
-        print("The given value is not an integer")
-Value1 = input("Please enter your first number : ")
-Value2 = input("Please enter your second number " )
-Value3 = int( Value1 ) + int( Value2 )
-addition(Value1, Value2)
-print("The type of user input is", type(Value3))
+
+firstNumber =float( input("Please enter your first number : "))
+secondNumber = float( input("Please enter your second number " ))
+answer = firstNumber  + secondNumber
+print("The addition of two numbers {} and {} is {}:".format(firstNumber, secondNumber, answer))
